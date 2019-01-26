96fada688bcc49d695edc4c096c79576e5147aa7
caoxing <164607840@qq.com>
2019-01-26 21:51:48 +0800

    创建敦煌App

---
+++ app/src/main/java/com/caoxing/dunhuang/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.caoxing.dunhuang
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
